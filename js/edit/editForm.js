60853f657127b3895f7538cdb85b514e5033796f
Luiz Gustavo <lgpossatto@gmail.com>
2021-07-08 16:43:46 -0300

    mod edit.js to post, put and delete

--- js/edit/editForm.js
+++ js/edit/editForm.js
@@ -1,88 +1,19 @@
-const data = [
-  {
-    "tech": {
-      "tech_list": [
-        "react",
-        "firebase",
-        "html",
-        "css",
-        "sass",
-        "javascript"
-      ],
-      "stack": "frontend",
-      "primary_tech": "react",
-      "secondary_tech": "firebase"
-    },
-    "_id": "60ddd27bbc81ad23eb1f7f5f",
-    "name": "Twitter Clone",
-    "desc": "A Twitter clone site made using react with hooks and context-api, plus firebase",
-    "git_url": "https://github.com/LGPossatto/twitter-clone-with-react-and-firebase",
-    "site_url": "https://lg-twitter-clone.netlify.app/",
-    "createdAt": "2021-07-01T14:34:35.125Z",
-    "updatedAt": "2021-07-01T14:34:35.125Z",
-    "__v": 0
-  },
-  {
-    "tech": {
-      "tech_list": [
-        "react",
-        "redux",
-        "html",
-        "css",
-        "sass",
-        "javascript"
-      ],
-      "stack": "frontend",
-      "primary_tech": "react",
-      "secondary_tech": "redux"
-    },
-    "_id": "60ddd39cbc81ad23eb1f7f61",
-    "name": "News Site",
-    "desc": "A news website made using react with hooks and redux",
-    "git_url": "https://github.com/LGPossatto/news-site",
-    "site_url": "https://salaodenoticias.netlify.app/",
-    "createdAt": "2021-07-01T14:39:24.860Z",
-    "updatedAt": "2021-07-01T14:39:24.860Z",
-    "__v": 0
-  },
-  {
-    "tech": {
-      "tech_list": [
-        "nodejs",
-        "express",
-        "html",
-        "css",
-        "sass",
-        "javascript"
-      ],
-      "stack": "fullstack",
-      "primary_tech": "nodejs",
-      "secondary_tech": "mongodb"
-    },
-    "_id": "60ddd5ddbc81ad23eb1f7f63",
-    "name": "My Portfolio",
-    "desc": "Fullstack project with backend API connected to MongoDB",
-    "git_url": "https://github.com/LGPossatto/news-site",
-    "site_url": "https://salaodenoticias.netlify.app/",
-    "createdAt": "2021-07-01T14:49:01.499Z",
-    "updatedAt": "2021-07-01T14:49:01.499Z",
-    "__v": 0
-  }
-]
-
 const editform = document.getElementById("edit-form");
+const editProjectsList = document.getElementById("edit-projects-list");
 const editError = document.getElementById("edit-error");
-const editProjectsList = document.getElementById('edit-projects-list')
-const editWarning = document.getElementById('edit-warning')
+const editSuccess = document.getElementById("edit-success");
+const editWarning = document.getElementById("edit-warning");
 
 // btns
 const btnCreate = document.getElementById("btn-create");
 const btnEdit = document.getElementById("btn-edit");
-const btnEditDelete = document.getElementById("edit-btn-delete");
+const btnSubmit = document.getElementById("btn-submit");
+const btnDelete = document.getElementById("btn-delete");
 const btnWarnintDelete = document.getElementById("btn-warnint-delete");
 const btnWarnintCancel = document.getElementById("btn-warnint-cancel");
 
 // inputs
+const projectId = document.getElementById("project-id");
 const projectName = document.getElementById("project-name");
 const projectDescription = document.getElementById("project-description");
 const githubUrl = document.getElementById("github-url");
@@ -91,65 +22,50 @@ const techList = document.getElementById("tech-list");
 const radioStack = document.getElementById("radio-stack").children;
 const authKey = document.getElementById("auth-key");
 
-// change inputs values
-const editInputsValues = (name = '', description = '', gitUrl = '', webUrl = '', techs = '', techStack = '') => {
-  projectName.value = name
-projectDescription.value = description
-githubUrl.value = gitUrl
-siteUrl.value = webUrl
-techList.value = techs
+// display error message
+const displayErr = (msg) => {
+  editError.innerHTML = msg;
 
-for (let radio of radioStack) {
+  setTimeout(() => {
+    editError.innerHTML = "";
+  }, 3000);
+};
+
+const displaySucc = (msg) => {
+  editSuccess.innerHTML = msg;
+
+  setTimeout(() => {
+    editSuccess.innerHTML = "";
+  }, 3000);
+};
+
+// change inputs values
+const editInputsValues = (
+  id = "",
+  name = "",
+  description = "",
+  gitUrl = "",
+  webUrl = "",
+  techs = "",
+  techStack = ""
+) => {
+  projectId.value = id;
+  projectName.value = name;
+  projectDescription.value = description;
+  githubUrl.value = gitUrl;
+  siteUrl.value = webUrl;
+  techList.value = techs;
+  authKey.value = "";
+
+  for (let radio of radioStack) {
     if (radio.nodeName === "INPUT" && radio.value === techStack) {
-      radio.checked = true
+      radio.checked = true;
     }
   }
-}
+};
 
-// remove class from list item
-const removeLiActive = () => {
-      for (let el of editProjectsList.children) {
-        el.classList.remove('li-active')
-      }
-}
-
-// fetch data
-const fetchAndDisplayProjects = async () => {
-  //const res = await fetch('https://lg-portfolio-api.herokuapp.com/api/projects')
-  //const data = await res.json()
-  
-  for (let project of data) {
-    const projectLi = document.createElement('li');
-    projectLi.classList.add('fs-small', 'fc-light');
-    projectLi.innerText = project.name;
-
-    // add event to the element
-    projectLi.addEventListener('click', () => {
-removeLiActive();
-
-      projectLi.classList.add('li-active')
-
-      editInputsValues(project.name, project.desc, project.git_url, project.site_url, project.tech.tech_list, project.tech.stack)
-    })
-
-    editProjectsList.appendChild(projectLi)
-  }  
-}
-
-// run fetch function
-fetchAndDisplayProjects();
-
-// on form submit
-editform.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const name = projectName.value;
-  const desc = projectDescription.value;
-  const git_url = githubUrl.value;
-  const site_url = siteUrl.value;
-
-  let tech_list = techList.value.split(",");
-
+// get values from inputs
+const getInputsData = () => {
   let stack = null;
   for (let radio of radioStack) {
     if (radio.nodeName === "INPUT" && radio.checked) {
@@ -157,59 +73,205 @@ editform.addEventListener("submit", (e) => {
     }
   }
 
-  //name && desc && git_url && site_url && tech_list && stack && authKey
+  return {
+    name: projectName.value,
+    desc: projectDescription.value,
+    git_url: githubUrl.value,
+    site_url: siteUrl.value,
+    tech: {
+      tech_list: techList.value.split(","),
+      stack,
+    },
+  };
+};
 
-  if (true) {
-    console.log(name, desc, git_url, site_url, tech_list, stack);
+// remove class from list item
+const removeLiActive = () => {
+  for (let el of editProjectsList.children) {
+    el.classList.remove("li-active");
+  }
+};
 
-    if (btnCreate.classList.contains('switch-btn-active')){
-      console.log('create');
+// fetch data
+const fetchAndDisplayProjects = async () => {
+  try {
+    const res = await fetch(
+      "https://lg-portfolio-api.herokuapp.com/api/projects"
+    );
+    const data = await res.json();
+
+    for (let project of data.data) {
+      const projectLi = document.createElement("li");
+      projectLi.classList.add("fs-small", "fc-light");
+      projectLi.innerText = project.name;
+
+      // add event to the element
+      projectLi.addEventListener("click", () => {
+        removeLiActive();
+
+        projectLi.classList.add("li-active");
+
+        editInputsValues(
+          project._id,
+          project.name,
+          project.desc,
+          project.git_url,
+          project.site_url,
+          project.tech.tech_list,
+          project.tech.stack
+        );
+      });
+
+      editProjectsList.appendChild(projectLi);
+    }
+  } catch (err) {
+    console.error(err);
+    displayErr(err.message);
+  }
+};
+
+// run fetch function
+fetchAndDisplayProjects();
+
+// on form submit
+editform.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const inputsData = getInputsData();
+  console.log(inputsData, authKey.value);
+
+  if (
+    inputsData.name &&
+    inputsData.desc &&
+    inputsData.git_url &&
+    inputsData.site_url &&
+    inputsData.tech.tech_list &&
+    inputsData.tech.stack &&
+    authKey.value
+  ) {
+    if (btnCreate.classList.contains("switch-btn-active")) {
+      // if create
+      try {
+        const res = await fetch(
+          `https://lg-portfolio-api.herokuapp.com/api/projects/`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${authKey.value}`,
+            },
+            body: JSON.stringify(inputsData),
+          }
+        );
+        const data = await res.json();
+        console.log(data);
+
+        if (!data.success) {
+          throw new Error(data.error);
+        } else {
+          changeBtn(btnCreate);
+          displaySucc("Project Created!");
+        }
+      } catch (err) {
+        console.error(err);
+        displayErr(err.message);
+      }
     } else {
-      console.log('edit');
+      // if edit
+      try {
+        const res = await fetch(
+          `https://lg-portfolio-api.herokuapp.com/api/projects/${projectId.value}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${authKey.value}`,
+            },
+            body: JSON.stringify(inputsData),
+          }
+        );
+        const data = await res.json();
+        console.log(data);
+
+        if (!data.success) {
+          throw new Error(data.error);
+        } else {
+          changeBtn(btnCreate);
+          displaySucc("Project Edited!");
+        }
+      } catch (err) {
+        console.error(err);
+        displayErr(err.message);
+      }
     }
   } else {
-    editError.innerHTML = "Please fill every field!";
-
-    setTimeout(() => {
-      editError.innerHTML = "";
-    }, 3000);
+    displayErr("Please fill every field!");
   }
 });
 
 // btn utils
 const changeBtn = (e) => {
-  if (e.target.innerHTML.trim() === "Create") {
+  if (e.innerHTML.trim() === "Create") {
     btnCreate.classList.add("switch-btn-active");
     btnEdit.classList.remove("switch-btn-active");
 
-    btnEditDelete.style.display = 'none'
-    editProjectsList.style.display= 'none'
+    btnSubmit.firstElementChild.innerText = "Create Project";
+    btnDelete.style.display = "none";
+    editProjectsList.style.display = "none";
 
-    editInputsValues()
-removeLiActive();
-
+    editInputsValues();
+    removeLiActive();
   } else {
     btnCreate.classList.remove("switch-btn-active");
     btnEdit.classList.add("switch-btn-active");
 
-    btnEditDelete.style.display = 'block'
-    editProjectsList.style.display= 'flex'
+    btnSubmit.firstElementChild.innerText = "Edit Project";
+    btnDelete.style.display = "block";
+    editProjectsList.style.display = "flex";
   }
 };
 
-btnCreate.addEventListener("click", changeBtn);
-btnEdit.addEventListener("click", async (e) => {
-  changeBtn(e);
+btnCreate.addEventListener("click", (e) => {
+  changeBtn(e.target);
+});
+btnEdit.addEventListener("click", (e) => {
+  changeBtn(e.target);
 });
 
-btnEditDelete.addEventListener('click', () => {
-  editWarning.style.display = 'flex'
-})
+btnDelete.addEventListener("click", () => {
+  if (authKey.value) {
+    editWarning.style.display = "flex";
+  } else {
+    displayErr("Authorization field must be provided!");
+  }
+});
 
-btnWarnintDelete.addEventListener('click', () => {
-  console.log('delete');
-})
+btnWarnintDelete.addEventListener("click", async () => {
+  try {
+    const res = await fetch(
+      `https://lg-portfolio-api.herokuapp.com/api/projects/${projectId.value}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${authKey.value}`,
+        },
+      }
+    );
+    const data = await res.json();
 
-btnWarnintCancel.addEventListener('click', () => {
-  editWarning.style.display = 'none'
-})
+    if (!data.success) {
+      throw new Error(data.error);
+    } else {
+      editWarning.style.display = "none";
+      changeBtn(btnCreate);
+      displaySucc("Project Deleted!");
+    }
+  } catch (err) {
+    console.error(err);
+    displayErr(err.message);
+  }
+});
+
+btnWarnintCancel.addEventListener("click", () => {
+  editWarning.style.display = "none";
+});
